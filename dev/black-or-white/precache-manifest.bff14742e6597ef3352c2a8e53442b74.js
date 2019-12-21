531d50b4922bd49568d8e4c0ed5e06b734dd3d28
MasaoBlue <16271994+MasaoBlue@users.noreply.github.com>
2019-12-22 00:33:10 +0900

    Add black or white judgment

---
+++ dev/black-or-white/precache-manifest.bff14742e6597ef3352c2a8e53442b74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247ed8e4821195c013e3",
+    "url": "css/app.aa2f78fe.css"
+  },
+  {
+    "revision": "03369870a1915dd82768",
+    "url": "css/chunk-vendors.b6d3e487.css"
+  },
+  {
+    "revision": "1d5b21d5305924186c192cda66dc47b6",
+    "url": "index.html"
+  },
+  {
+    "revision": "247ed8e4821195c013e3",
+    "url": "js/app.49d2393d.js"
+  },
+  {
+    "revision": "03369870a1915dd82768",
+    "url": "js/chunk-vendors.66bc8eae.js"
+  },
+  {
+    "revision": "3ea01004107c80afdfb91db502b96063",
+    "url": "js/opencv.js"
+  },
+  {
+    "revision": "0a25812bb82d6811db243d1cae49b0d4",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
