202a637668635f2ec2034f1494e0598d7ad72437
MasaoBlue <16271994+MasaoBlue@users.noreply.github.com>
2021-05-29 18:25:13 +0900

    Update bistrover csv

---
+++ iidx-top-rankers-viewer/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/rankers":["static\u002Fchunks\u002Fpages\u002Frankers.js"],sortedPages:["\u002F_app","\u002F_error","\u002Frankers"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
